e1ac265e8cacfb6f533c03d01be5e1a42eb033b3
Soros Liu <soros.liu1029@gmail.com>
2025-06-08 22:27:40 +0800

    auto attach LeetCodeNotebookHeader to leetcode notebook

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import {
   ILayoutRestorer
 } from '@jupyterlab/application';
 import { ICommandPalette, WidgetTracker } from '@jupyterlab/apputils';
-import { IDocumentManager } from '@jupyterlab/docmanager';
+import {
+  IDocumentManager,
+  IDocumentWidgetOpener
+} from '@jupyterlab/docmanager';
 import { NotebookPanel } from '@jupyterlab/notebook';
 
 import { LeetCodeMainWidget, LeetCodeHeaderWidget } from './widget';
@@ -18,12 +21,13 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: PLUGIN_ID,
   description: 'Integrate LeetCode into beloved Jupyter.',
   autoStart: true,
-  requires: [ICommandPalette, IDocumentManager],
+  requires: [ICommandPalette, IDocumentManager, IDocumentWidgetOpener],
   optional: [ILayoutRestorer],
   activate: (
     app: JupyterFrontEnd,
     palette: ICommandPalette,
     docManager: IDocumentManager,
+    docWidgetOpener: IDocumentWidgetOpener,
     restorer: ILayoutRestorer | null
   ) => {
     let leetcodeWidget: LeetCodeMainWidget;
@@ -45,28 +49,26 @@ const plugin: JupyterFrontEndPlugin<void> = {
       }
     });
     palette.addItem({ command, category: 'LeetCode' });
-
-    const addHeaderCommand = 'leetcode-widget:add-header';
-    app.commands.addCommand(addHeaderCommand, {
-      label: 'Add Header to LeetCode Widget',
-      caption: 'Add Header to LeetCode Widget',
-      execute: () => {
-        const main = app.shell.currentWidget;
-        if (main instanceof NotebookPanel) {
-          const widget = new LeetCodeHeaderWidget(main);
-          widget.node.style.minHeight = '20px';
-          main.contentHeader.addWidget(widget);
-        }
-      }
-    });
-    palette.addItem({ command: addHeaderCommand, category: 'LeetCode' });
-
     const tracker = new WidgetTracker<LeetCodeMainWidget>({
       namespace: 'leetcode-widget'
     });
     if (restorer) {
       restorer.restore(tracker, { command, name: () => 'leetcode' });
     }
+
+    docWidgetOpener.opened.connect((sender, widget) => {
+      if (widget instanceof NotebookPanel) {
+        widget.revealed.then(() => {
+          if (widget.model?.metadata?.leetcode_question_info) {
+            const header = new LeetCodeHeaderWidget(widget);
+            header.node.style.minHeight = '20px';
+            if (widget.contentHeader.widgets.every(w => w.id !== header.id)) {
+              widget.contentHeader.addWidget(header);
+            }
+          }
+        });
+      }
+    });
   }
 };
 
